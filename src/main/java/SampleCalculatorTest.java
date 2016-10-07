1a068e45115a03e18d723b9139e5d2b844f8ee8a
Jasmi <jasmine.shaheer@gmail.com>
2016-10-07 14:08:35 +0200

    "This is my third commit. I added test cases for subtraction method."

--- src/main/java/SampleCalculatorTest.java
+++ src/main/java/SampleCalculatorTest.java
@@ -22,8 +22,115 @@ public class SampleCalculatorTest {
 		result=firstNumber+secondNumber;
 		LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
 		assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+		}
 		
 	}
+	
+		public void testAddMediumSizedPositiveNumbers() {
+			double firstNumber=0;
+			double secondNumber=0;
+			double result=0;
+			for(int i=0;i<200;i++){
+			firstNumber=Double.valueOf(df.format(r.nextDouble()*100));
+			secondNumber=Double.valueOf(df.format(r.nextDouble()*100));
+			result=firstNumber+secondNumber;
+			LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
+			assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+			}
+		}
+			
 
-}
+			
+			public void testAddLargeSizedPositiveNumbers() {
+				double firstNumber=0;
+				double secondNumber=0;
+				double result=0;
+				for(int i=0;i<200;i++){
+				firstNumber=Double.valueOf(df.format(r.nextDouble()*1000));
+				secondNumber=Double.valueOf(df.format(r.nextDouble()*1000));
+				result=firstNumber+secondNumber;
+				LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
+				assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+				}
+			}
+				
+
+				public void testAddSmallSizedNegativeNumbers() {
+					double firstNumber=0;
+					double secondNumber=0;
+					double result=0;
+					for(int i=0;i<200;i++){
+					firstNumber=Double.valueOf(df.format(r.nextDouble()*-1000));
+					secondNumber=Double.valueOf(df.format(r.nextDouble()*-1000));
+					result=firstNumber+secondNumber;
+					LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
+					assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+					}
+				}
+				
+					public void testAddMediumSizedNegativeNumbers() {
+						double firstNumber=0;
+						double secondNumber=0;
+						double result=0;
+						for(int i=0;i<200;i++){
+						firstNumber=Double.valueOf(df.format(r.nextDouble()*-100));
+						secondNumber=Double.valueOf(df.format(r.nextDouble()*-100));
+						result=firstNumber+secondNumber;
+						LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
+						assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+						}
+					}
+						
+					
+					
+		
+
+                      public void testAddLargeSizedNegativeNumbers() {
+						double firstNumber=0;
+						double secondNumber=0;
+						double result=0;
+						for(int i=0;i<200;i++){
+						firstNumber=Double.valueOf(df.format(r.nextDouble()*-10));
+						secondNumber=Double.valueOf(df.format(r.nextDouble()*-10));
+						result=firstNumber+secondNumber;
+						LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
+						assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+						}
+                        }
+                      public void testSubtractSmallSizedpositiveNumbers() {
+  						double firstNumber=0;
+  						double secondNumber=0;
+  						double result=0;
+  						for(int i=0;i<200;i++){
+  						firstNumber=Double.valueOf(df.format(r.nextDouble()*10));
+  						secondNumber=Double.valueOf(df.format(r.nextDouble()*10));
+  						result=firstNumber-secondNumber;
+  						LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
+  						assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+  						}
+                          }
+                      public void testSubtractMediumSizedPositiveNumbers() {
+  						double firstNumber=0;
+  						double secondNumber=0;
+  						double result=0;
+  						for(int i=0;i<200;i++){
+  						firstNumber=Double.valueOf(df.format(r.nextDouble()*100));
+  						secondNumber=Double.valueOf(df.format(r.nextDouble()*100));
+  						result=firstNumber-secondNumber;
+  						LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
+  						assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+  						}
+                          }
+                      public void testSubtractLargeSizedPositiveNumbers() {
+  						double firstNumber=0;
+  						double secondNumber=0;
+  						double result=0;
+  						for(int i=0;i<200;i++){
+  						firstNumber=Double.valueOf(df.format(r.nextDouble()*1000));
+  						secondNumber=Double.valueOf(df.format(r.nextDouble()*1000));
+  						result=firstNumber-secondNumber;
+  						LOG.info("Testing the method add with"+firstNumber+"and"+secondNumber);
+  						assertEquals(Math.round(sc.add(firstNumber, secondNumber)),Math.round(result));
+  						}
+                          }
 }
